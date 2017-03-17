d71df8405b5ad38fbc994ac5e46c596c371f1992
amarendra1507 <amarendra1507@gmail.com>
2017-03-17 19:37:31 +0530

    Add files via upload
    This program only prints the primitive data types of java

---
+++ JavaType.java
@@ -0,0 +1,35 @@
+class JavaType{
+	public static void main(String[] args){
+		
+
+		System.out.println("Java Type (Premitive)");
+
+		System.out.println("Size of Byte : " + Byte.SIZE + "Bit");
+		System.out.println("Minimum of Byte : " + Byte.MIN_VALUE);
+		System.out.println("Maximum of Byte : " + Byte.MAX_VALUE);
+		//System.out.println("Size of Character : " + char.SIZE);
+		System.out.println("Size of Long : " + Long.SIZE + "Bit");
+		System.out.println("Minimum of Long : " + Long.MIN_VALUE);
+		System.out.println("Maximum of Long : " + Long.MAX_VALUE);
+
+		System.out.println("Size of Short : " + Short.SIZE + "Bit");
+		System.out.println("Minimum of Short : " + Short.MIN_VALUE);
+		System.out.println("Maximum of Short : " + Short.MAX_VALUE);
+
+		System.out.println("Size of Float : " + Float.SIZE + "Bit");
+		System.out.println("Minimum of Float : " + Float.MIN_VALUE);
+		System.out.println("Maximum of Float : " + Float.MAX_VALUE);
+
+		System.out.println("Size of Integer : " + Integer.SIZE + "Bit");
+		System.out.println("Minimum of Integer : " + Integer.MIN_VALUE);
+		System.out.println("Maximum of Integer : " + Integer.MAX_VALUE);
+
+		//System.out.println("Size of Boolean : " + BOOLEAN.SIZE);
+		System.out.println("Size of Double : " + Double.SIZE + "Bit");
+		System.out.println("Minimum of Double : " + Double.MIN_VALUE);
+		System.out.println("Maximum of Double : " + Double.MAX_VALUE);
+
+
+	}
+		
+}
